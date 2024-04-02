560da7d3050f5e7936cb39ee081d13feac990339
Rajashree <rajashreesrinivasan2@gmail.com>
2024-04-02 15:28:27 +0530

    Updated sender email id

--- src/php/otpVerification/otp.php
+++ src/php/otpVerification/otp.php
@@ -2,27 +2,21 @@
 include('../db/dbconfig.php');
 include('../headers/headerconfig.php');
 
-$data=json_decode(file_get_contents('php://input'));
+$data = json_decode(file_get_contents('php://input'));
 
-$email=$data->email;
-$otp=rand(100000,999999);
-$msg="Your OTP is ".$otp."\nThis is a verified message for ".$email." for password change.\n Please ignore this message if you don't send it.";
-$sub="OTP for Password Change from Agri AI";
-$headers="From: galaxymessenger15@gmail.com";
+$email = $data->email;
+$otp = rand(100000, 999999);
+$msg = "Your OTP is " . $otp . "\nThis is a verified message for " . $email . " for password change.\n Please ignore this message if you don't send it.";
+$sub = "OTP for Password Change from Agri AI";
+$headers = "From: agriwithai@gmail.com";
 // here give your email address in headers
-try
-{
-    $sendotp=mail($email,$sub,$msg,$headers);
-    if($sendotp)
-    {
-        echo json_encode(['msg'=>"otp sent",'otp'=>$otp]);
+try {
+    $sendotp = mail($email, $sub, $msg, $headers);
+    if ($sendotp) {
+        echo json_encode(['msg' => "otp sent", 'otp' => $otp]);
+    } else {
+        echo json_encode(['msg' => "error sending otp"]);
     }
-    else
-    {
-        echo json_encode(['msg'=>"error sending otp"]);
-    }
-}
-catch(Exception $e)
-{
-    json_encode(['msg'=>'exception'.$e]);
+} catch (Exception $e) {
+    json_encode(['msg' => 'exception' . $e]);
 }
